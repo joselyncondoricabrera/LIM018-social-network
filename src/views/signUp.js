bae83b0ea926dfc9569820ed579e8323e8f54f9f
camotito0 <cecilianallerena@gmail.com>
2022-07-05 12:00:30 -0500

    Añadiendo estilos a login, signup views, se añadieron imagenes de fondo, archivos css y archivos ttf para los fonts

--- src/views/signUp.js
+++ src/views/signUp.js
@@ -1,23 +1,26 @@
 function signUp () {
     const signup = `
-    <h1 class="form-create" >Crear cuenta</h1>
-    <p>Por favor llene los datos antes de continuar.</p>
-    <form class="create-account">
-        <div class="create-input" >
-            <img src="./icons/user.png"/>
-            <input class="username" type="text" placeholder="nombre de usuario" required>
-        </div>
-        <div class="create-input">
-            <img src="./icons/email.png"/>
-            <input class="email" type="email" placeholder="email" required>
-        </div>
-        <div class="create-input">
-            <img src="./icons/key.png"/>
-            <input class="password" type="password" placeholder="contraseña" required>
-        </div>
-        <button class="form-create-account-button" type="button">Enviar</button>
-        <a href="/">tienes cuenta</a>
-    </form>
+    <img class="background" src="./icons/mobile-createA.png" />
+    <main class="main-form">
+        <h1>Crear cuenta</h1>
+        <p>Por favor llene los datos antes de continuar.</p>
+        <form class="form">
+            <div class="form-input" >
+                <img src="./icons/user.png"/>
+                <input class="username" type="text" placeholder="nombre de usuario" required>
+            </div>
+            <div class="form-input">
+                <img src="./icons/email.png"/>
+                <input class="email" type="email" placeholder="email" required>
+            </div>
+            <div class="form-input">
+                <img src="./icons/key.png"/>
+                <input class="password" type="password" placeholder="contraseña" required>
+            </div>
+            <button class="form-button" type="button">Crear cuenta</button>
+            <a href="/" class="form-link" >tienes cuenta?</a>
+        </form>
+    </main>
     `
     const element = document.querySelector('body');
     element.innerHTML = signup;
